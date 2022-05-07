15d667a6c5a2b660eb49e3cc3370af7c8169f87c
denisfominykh <2028902d>
2022-05-08 00:10:18 +0300

    feat: add api and router paths

---
+++ src/constants/routerPaths.ts
@@ -0,0 +1,17 @@
+export const ROUTER_PATHS = {
+  HOME: {
+    _: '/',
+  },
+  PEOPLE: {
+    _: '/people',
+    BY_ID: '/people/:id',
+  },
+  PLANETS: {
+    _: '/planets',
+    BY_ID: '/planets/:id',
+  },
+  STARSHIPS: {
+    _: '/starships',
+    BY_ID: '/starships/:id',
+  },
+};
